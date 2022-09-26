e355ef28529e4df8c7c234b25f191ad6e20b0ca0
kevinrss01 <kevin.rousseau3@gmail.com>
2022-09-26 17:37:55 +0200

    delete page home

--- Test_technique/src/routes/index.js
+++ Test_technique/src/routes/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Homepage from '../Views/Homepage/homePage';
 import Login from '../Views/Login/login';
 import Movies from '../Views/Movies/movies';
 
 const Router = () => (
 	<BrowserRouter>
 		<Routes>
-			<Route path="/" element={<Homepage />} />
+			<Route path="/" element={<Login />} />
 			<Route path="login" element={<Login />} />
 			<Route path="movies-list" element={<Movies />} />
 			<Route path="*" element={<Navigate to="/" />} />
